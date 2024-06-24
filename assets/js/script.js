42ce39d77bebab43090635327106a4f5cf197db0
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-06-24 09:49:35 +0100

    pulled source as fault displaying Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
-document.getElementById("submit").addEventListener("click", e => postForm(e));
+// document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function getStatus(e) {
     e.preventDefault();
@@ -11,11 +11,23 @@ async function getStatus(e) {
     const response = await fetch(queryString);
     const data = await response.json();
     if (response.ok) {
-        console.log(data.expiry);
+        console.log(data);
     } else {
         throw new Error(data.error);
     }    
 };
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div> Your key is valid until </div>`;
+    results += `<div class="key-status>${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
 
 
 
