3d529fbf100da4820c76c44a61ca03e5e1a694d0
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-06-24 10:00:46 +0100

    found errors in my js.script used source code to correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,65 +3,31 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
-// document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function getStatus(e) {
-    e.preventDefault();
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
+
     const response = await fetch(queryString);
+
     const data = await response.json();
+
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
     } else {
         throw new Error(data.error);
-    }    
-};
+    }
+
+}
 
 function displayStatus(data) {
 
     let heading = "API Key Status";
-    let results = `<div> Your key is valid until </div>`;
-    results += `<div class="key-status>${data.expiry}</div>`;
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
-
     resultsModal.show();
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch(`https://ci-jshint.herokuapp.com/api?api_key=${apiKey}`, {
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "application/json",
-//         "Authorization": `Bearer ${apiKey}`
-//     }
-// })
-//     .then(response => {
-//         if (response.ok) {
-//             return response.json();
-//         } else {
-//             throw new Error("Response was not successful.");
-//         }
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     });
